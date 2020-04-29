7154c973ceeca6e58e3610f3d9190aade16c9d67
suravimandal <suravi.mandal@rediffmail.com>
2020-04-29 19:05:44 +0800

    deprecated command removed from gradle.build

--- build/resources/main/db/migration/V2__users.sql
+++
@@ -1,9 +0,0 @@
- CREATE TABLE users
-   (
-      id    BIGSERIAL NOT NULL,
-      user_id   BIGSERIAL   NOT NULL,
-      user_name VARCHAR(255) NOT NULL,
-
-      user_addr VARCHAR(255) NOT NULL,
-      user_email VARCHAR(255) NOT NULL
-  );
